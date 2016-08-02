5e369bbb1fd202c4a6ddb5e51adf05f824332600
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-02 14:34:33 +0800

    ver 1.0

--- iAdIntegration/iAdIntegration/ViewController.h
+++ iAdIntegration/iAdIntegration/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<ADBannerViewDelegate> {
+    ADBannerView *bannerView;
+}
 
 
 @end
--- iAdIntegration/iAdIntegration/ViewController.m
+++ iAdIntegration/iAdIntegration/ViewController.m
@@ -16,12 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    bannerView = [[ADBannerView alloc]initWithFrame: CGRectMake(0, 0, 320, 50)];
+    bannerView.delegate = self;
+    
+    // Optional to set background color to clear color
+    [bannerView setBackgroundColor:[UIColor blackColor]];
+    [self.view addSubview: bannerView];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - AdViewDelegates
+
+-(void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
+    NSLog(@"Error loading");
+}
+
+-(void)bannerViewDidLoadAd:(ADBannerView *)banner {
+    NSLog(@"Ad loaded");
+}
+
+-(void)bannerViewWillLoadAd:(ADBannerView *)banner {
+    NSLog(@"Ad will load");
+}
+
+-(void)bannerViewActionDidFinish:(ADBannerView *)banner {
+    NSLog(@"Ad did finish");
 }
 
 @end
